0eeccd3d5815d810c912f4e3eb2593a361069890
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2024-07-12 00:37:40 +0300

    ahhh

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,18 @@ import ColorPicker from "./ColorPicker";
 import axios from "axios";
 import { IoMdClose } from "react-icons/io";
 import "./EntsAttrsTable.css"; // Assuming you have a CSS file for styling
-import { createTheme,ThemeProvider } from '@mui/material/styles';
-import { green, purple } from '@mui/material/colors';
-import backgroundImage from './assets/background.jpg';
-import CircularProgress from '@mui/material/CircularProgress';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
+import backgroundImage from "./assets/background.jpg";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#a819d3',
+      main: "#a819d3",
     },
     secondary: {
-      main:'#ff7100',
+      main: "#ff7100",
     },
   },
 });
@@ -94,7 +94,7 @@ export default function FormDialog() {
 
       const data = await response.json();
       setResult(data.result);
-      
+
       setError(false);
     } catch (error) {
       console.error("There was an error processing the text!", error);
@@ -117,7 +117,7 @@ export default function FormDialog() {
 
   const handleDialogSubmit = async (e) => {
     e.preventDefault();
-
+    setShowEntsOnly(false);
     if (!databaseDescription.trim()) {
       setError(true);
       return;
@@ -141,7 +141,6 @@ export default function FormDialog() {
       setEntitiesWithPKs(data.entities_with_pks);
       setRelationships(data.relationships);
       setResult([]); // Clear the result state
-      setShowEntsOnly(false);
     } catch (error) {
       console.error("There was an error processing the data!", error);
     }
@@ -187,238 +186,250 @@ export default function FormDialog() {
     console.log("Selected color in App:", color);
     // You can perform further actions with the selected color here
   };
-  
+
   const toggleColorPicker = () => {
     setShowColorPicker((prevShowColorPicker) => !prevShowColorPicker);
   };
 
   const divStyle = {
     backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    height: '100vh',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    height: "100vh",
     // overflow: 'hidden',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: '20px', // Adjust the gap as needed
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "20px", // Adjust the gap as needed
   };
 
   const textFieldStyle = {
-    width: '50%', // Adjust the width as needed
-    borderColor: error ? "red" : "" 
+    width: "50%", // Adjust the width as needed
+    borderColor: error ? "red" : "",
   };
-
 
   return (
     <div className="App" style={divStyle}>
-    <ThemeProvider theme={theme}>
-    <React.Fragment>
-      <h1 style={{ color: "#a819d3", textAlign: "center" }}>Webby</h1>
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <h1 style={{ color: "#a819d3", textAlign: "center" }}>Webby</h1>
 
-      <TextField
-        id="standard-basic-2"
-        label="Database Description"
-        variant="standard"
-        
-        multiline
-        maxRows={10}
-        error={error}
-        onChange={(e) => {
-          setDatabaseDescription(e.target.value);
-          setError(false);
-        }}
-        value={databaseDescription}
-        sx={textFieldStyle}
-      />
+          <TextField
+            id="standard-basic-2"
+            label="Database Description"
+            variant="standard"
+            multiline
+            maxRows={10}
+            error={error}
+            onChange={(e) => {
+              setDatabaseDescription(e.target.value);
+              setError(false);
+            }}
+            value={databaseDescription}
+            sx={textFieldStyle}
+          />
 
-      <TextField
-        id="standard-basic-1"
-        label="GUI Description"
-        variant="standard"
-        fullWidth
-        multiline
-        maxRows={10}
-        sx={textFieldStyle}
-      />
-      <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-
-        
-        <Button  color="primary" onClick={toggleColorPicker}>
-              {showColorPicker ? 'Close' : 'Open'} Color Picker
+          <TextField
+            id="standard-basic-1"
+            label="GUI Description"
+            variant="standard"
+            fullWidth
+            multiline
+            maxRows={10}
+            sx={textFieldStyle}
+          />
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <Button color="primary" onClick={toggleColorPicker}>
+              {showColorPicker ? "Close" : "Open"} Color Picker
             </Button>
-            
-      </div>
-      {showColorPicker ? <ColorPicker onColorChange={handleColorChange} />:<></>}
-      <Button variant="contained" onClick={handleSubmit}>
-        Submit
-      </Button>
-
-      <Dialog
-        open={openDialog}
-        onClose={handleCloseDialog}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title"><h2>Customize your database</h2></DialogTitle>
-        <DialogContent>
-          {showEntsOnly ? (
-            <>
-              {result.length > 0 ? (
-                <>
-                <h2>Tabels names</h2>
-                <ul>
-                  {result.map((item, index) => (
-                    <li key={index}>
-                      {item}
-                      {!hideDeleteIcons && (
-                        <MdDeleteForever
-                          onClick={() => handleDeleteElement(index)}
-                          onMouseEnter={(e) => (e.target.style.color = "red")}
-                          onMouseLeave={(e) => (e.target.style.color = "black")}
-                          style={{ cursor: "pointer", marginLeft: "10px" }}
-                        />
-                      )}
-                    </li>
-                  ))}
-                </ul>
-                <TextField
-                margin="dense"
-                id="newElement"
-                label="New Table"
-                fullWidth
-                variant="standard"
-                value={newElement}
-                onChange={(e) => setNewElement(e.target.value)}
-              />
-              <Button onClick={handleAddElement} color="primary">
-                Add
-              </Button>
-                </>
-              ) : (
-                <DialogContentText>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}>
-                  <CircularProgress color="primary" />
-                </div>
-              </DialogContentText>
-              )}
-              
-            </>
+          </div>
+          {showColorPicker ? (
+            <ColorPicker onColorChange={handleColorChange} />
           ) : (
-            <div>
-              
-              {isEmptyObject(entitiesWithAttr) ? (
-                <p>No entities with attributes found</p>
-              ) : (
-                <div>
-                  <h2>Tables and Columns</h2>
-                  {isEmptyObject(entitiesWithAttr) ? (
-                    <p>No entities with attributes found</p>
-                  ) : (
-                    <ul>
-                      {Object.keys(entitiesWithAttr).map((entity, index) => (
-                        <li key={index} style={{ padding: '10px' }}>
-                          <div>
-                            <strong>{entity}</strong>:
-                            <div className="attributes-row">
-                              {entitiesWithAttr[entity].map(
-                                (attr, attrIndex) => (
-                                  <div
-                                    className={`attribute ${
-                                      isPrimaryKey(entity, attr)
-                                        ? "primary-key"
-                                        : ""
-                                    } ${
-                                      isForeignKey(entity, attr)
-                                        ? "foreign-key"
-                                        : ""
-                                    }`}
-                                    key={attrIndex}
-                                  >
-                                    {attr}{" "}
-                                    {isPrimaryKey(entity, attr) ? "(pk)" : ""}{" "}
-                                    {isForeignKey(entity, attr) ? "(fk)" : ""}
-                                    {!isPrimaryKey(entity, attr) &&
-                                      !isForeignKey(entity, attr) && (
-                                        <IoMdClose
-                                          className="close-icon"
-                                          onClick={() =>
-                                            handleDelete(entity, attr)
-                                          }
-                                        />
-                                      )}
-                                  </div>
-                                )
-                              )}
-                            </div>
-                            <div className="add-attribute">
-                              <TextField
-                                type="text"
-                                label="New Column"
-                                variant="standard"
-                                onKeyDown={(e) => {
-                                  if (e.key === "Enter") {
-                                    handleAdd(entity, e.target.value);
-                                    e.target.value = "";
-                                  }
+            <></>
+          )}
+          <Button variant="contained" onClick={handleSubmit}>
+            Submit
+          </Button>
+
+          <Dialog
+            open={openDialog}
+            onClose={handleCloseDialog}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">
+              <h2>Customize your database</h2>
+            </DialogTitle>
+            <DialogContent>
+              {showEntsOnly ? (
+                <>
+                  {result.length > 0 ? (
+                    <>
+                      <h2>Tabels names</h2>
+                      <ul>
+                        {result.map((item, index) => (
+                          <li key={index}>
+                            {item}
+                            {!hideDeleteIcons && (
+                              <MdDeleteForever
+                                onClick={() => handleDeleteElement(index)}
+                                onMouseEnter={(e) =>
+                                  (e.target.style.color = "red")
+                                }
+                                onMouseLeave={(e) =>
+                                  (e.target.style.color = "black")
+                                }
+                                style={{
+                                  cursor: "pointer",
+                                  marginLeft: "10px",
                                 }}
                               />
-                              <Button
-                                onClick={(e) => {
-                                  const input = e.target.previousSibling;
-                                  handleAdd(entity, input.value);
-                                  input.value = "";
-                                }}
-                              >
-                                Add
-                              </Button>
+                            )}
+                          </li>
+                        ))}
+                      </ul>
+                      <TextField
+                        margin="dense"
+                        id="newElement"
+                        label="New Table"
+                        fullWidth
+                        variant="standard"
+                        value={newElement}
+                        onChange={(e) => setNewElement(e.target.value)}
+                      />
+                      <Button onClick={handleAddElement} color="primary">
+                        Add
+                      </Button>
+                    </>
+                  ) : (
+                    <DialogContentText>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          height: "100px",
+                        }}
+                      >
+                        <CircularProgress color="primary" />
+                      </div>
+                    </DialogContentText>
+                  )}
+                </>
+              ) : (
+                <div>
+                  {isEmptyObject(entitiesWithAttr) ? (
+                    <DialogContentText>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          height: "100px",
+                        }}
+                      >
+                        <CircularProgress color="primary" />
+                      </div>
+                    </DialogContentText>
+                  ) : (
+                    <div>
+                      <h2>Tables and Columns</h2>
+
+                      <ul>
+                        {Object.keys(entitiesWithAttr).map((entity, index) => (
+                          <li key={index} style={{ padding: "10px" }}>
+                            <div>
+                              <strong>{entity}</strong>:
+                              <div className="attributes-row">
+                                {entitiesWithAttr[entity].map(
+                                  (attr, attrIndex) => (
+                                    <div
+                                      className={`attribute ${
+                                        isPrimaryKey(entity, attr)
+                                          ? "primary-key"
+                                          : ""
+                                      } ${
+                                        isForeignKey(entity, attr)
+                                          ? "foreign-key"
+                                          : ""
+                                      }`}
+                                      key={attrIndex}
+                                    >
+                                      {attr}{" "}
+                                      {isPrimaryKey(entity, attr) ? "(pk)" : ""}{" "}
+                                      {isForeignKey(entity, attr) ? "(fk)" : ""}
+                                      {!isPrimaryKey(entity, attr) &&
+                                        !isForeignKey(entity, attr) && (
+                                          <IoMdClose
+                                            className="close-icon"
+                                            onClick={() =>
+                                              handleDelete(entity, attr)
+                                            }
+                                          />
+                                        )}
+                                    </div>
+                                  )
+                                )}
+                              </div>
+                              <div className="add-attribute">
+                                <TextField
+                                  type="text"
+                                  label="New Column"
+                                  variant="standard"
+                                  onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                      handleAdd(entity, e.target.value);
+                                      e.target.value = "";
+                                    }
+                                  }}
+                                />
+
+                              </div>
                             </div>
-                          </div>
-                        </li>
-                      ))}
-                    </ul>
+                          </li>
+                        ))}
+                      </ul>
+                      {relationships.length > 0 ? (
+                        <>
+                          <h2>Extra Tables for Relationships</h2>
+                          <ul>
+                            {relationships.map((relationship, index) =>
+                              relationship[0] === "many" &&
+                              relationship[2] === "many" ? (
+                                <li key={index}>
+                                  {relationship[1]}_{relationship[3]}
+                                </li>
+                              ) : null
+                            )}
+                          </ul>
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
                   )}
                 </div>
               )}
-
-              <h2>Extra Tables for Relationships</h2>
-              {relationships.length > 0 ? (
-                <>
-                  <ul>
-                    {relationships.map((relationship, index) =>
-                      relationship[0] === "many" &&
-                      relationship[2] === "many" ? (
-                        <li key={index}>
-                          {relationship[1]}_{relationship[3]}
-                        </li>
-                      ) : null
-                    )}
-                  </ul>
-                </>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog} color="primary">
+                Close
+              </Button>
+              {showEntsOnly ? (
+                <Button onClick={handleDialogSubmit} color="primary">
+                  Submit
+                </Button>
               ) : (
-                <p>No results to display.</p>
+                <Button onClick={handleSave} color="primary">
+                  Ok
+                </Button>
               )}
-            </div>
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">
-            Close
-          </Button>
-          {showEntsOnly ? (
-            <Button onClick={handleDialogSubmit} color="primary">
-              Submit
-            </Button>
-          ) : (
-            <Button onClick={handleSave} color="primary">
-              Ok
-            </Button>
-          )}
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
-    </ThemeProvider>
+            </DialogActions>
+          </Dialog>
+        </React.Fragment>
+      </ThemeProvider>
     </div>
   );
 }
